9a3caeced0789659b88e0860085192b86df18daf
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-08-11 15:53:25 +0600

    Splash Complete.

--- app/src/main/java/com/wordpress/mortuza99/multiplicationfun/Splash.java
+++ app/src/main/java/com/wordpress/mortuza99/multiplicationfun/Splash.java
@@ -1,13 +1,31 @@
 package com.wordpress.mortuza99.multiplicationfun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
 
+    private static final int delay = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread t = new Thread() {
+            public void run() {
+                try {
+                    sleep(delay);
+                    finish();
+                    Intent cv = new Intent(Splash.this, MainActivity.class);
+                    startActivity(cv);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        t.start();
+
     }
 }
